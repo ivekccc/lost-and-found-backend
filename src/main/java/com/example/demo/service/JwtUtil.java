3032ec37de05b526eca3b342a1db795e29b53b2d
ivan <ivanjelisavcic6@gmail.com>
2025-12-24 20:54:45 +0100

    Vraćeno trajanje tokena na 10 sati, uklonjeni svi logovi

--- src/main/java/com/example/demo/service/JwtUtil.java
+++ src/main/java/com/example/demo/service/JwtUtil.java
@@ -9,25 +9,28 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
 
 @Component
+
 public class JwtUtil {
 
     @Value("${jwt.secret}")
     private String SECRET;
 
     public String generateToken(String username) {
+        long expiresInMs = 1000 * 60 * 60 * 10; // 10 sati
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10h
+                .setExpiration(new Date(System.currentTimeMillis() + expiresInMs))
                 .signWith(SignatureAlgorithm.HS256, SECRET)
                 .compact();
     }
 
     public String generateRefreshToken(String username) {
+        long expiresInMs = 1000L * 60 * 60 * 24 * 7; // 7 dana
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 7)) // 7 dana
+                .setExpiration(new Date(System.currentTimeMillis() + expiresInMs))
                 .signWith(SignatureAlgorithm.HS256, SECRET)
                 .compact();
     }
