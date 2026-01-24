da1a478f8c41d3b41ba9b888d2e67fdbfd61f753
ivanjelisavcic <ivanjelisavcic6@gmail.com>
2026-01-24 14:38:16 +0100

    Add @Transactional to register method in AuthService
    Annotated the register method with @Transactional to ensure atomicity during user registration. This helps maintain data consistency in case of errors during the registration process.

--- src/main/java/com/example/demo/service/AuthService.java
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AuthService {
@@ -36,6 +37,7 @@ public class AuthService {
         this.userDetailsService = userDetailsService;
     }
 
+    @Transactional
     public AuthResponseDTO register(RegisterRequestDto req) {
         if (userRepository.findByEmail(req.getEmail()).isPresent()) {
             throw new UserAlreadyExistsException("Email already exists");
